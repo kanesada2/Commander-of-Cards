87bd7ca3dc5da9a91c7626c889be7350702a92d8
kanesada2 <kanesada2@users.noreply.github.com>
2020-03-06 03:40:48 +0900

    UniRXことはじめ

---
+++ Assets/PhaseReactiveProperty.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public enum Phase
+{
+DRAW,
+SELECT_ATTACKER,
+SELECT_HAND,
+USE,
+BATTLE
+}
+
+[System.Serializable]
+public class PhaseReactiveProperty : ReactiveProperty
+{
+public PhaseReactiveProperty() { }
+public PhaseReactiveProperty(Phase initialValue) : base(initialValue) { }
+
+}
