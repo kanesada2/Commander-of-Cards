b59c34883ba4be412bdc77aafd0174829d518aa2
kanesada2 <kanesada2@users.noreply.github.com>
2020-03-09 01:38:56 +0900

    見た目面の修正とカード制御スクリプト仮置き

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Card : MonoBehaviour
 {
-    public GameObject hands;
     // Start is called before the first frame update
     void Start()
     {
